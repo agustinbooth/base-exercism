07bb4a70509ab1a0861fc25ded1f45bd08cf600c
agustinvd10 <avalenzu@chicagobooth.edu>
2021-04-09 01:19:37 +0000

    starting Acronym

--- ruby/acronym/acronym.rb
+++ ruby/acronym/acronym.rb
@@ -7,8 +7,19 @@ To get started with TDD, see the `README.md` file in your
 `ruby/acronym` directory.
 =end
 
-class Acronym < Minitest::Test
+class Acronym
+
   def self.abbreviate(sentence)
-    "PNG"
+    
+    sentence = sentence.gsub(/[^a-z0-9\s]/i, "")
+    
+    each_word = sentence.split
+
+    each_word.each do |a_word|
+        @acronym << a_word[0]
+    end
+    
+    return @acronym
+
   end
 end
