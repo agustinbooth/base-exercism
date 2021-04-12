a42a20a85652040400b62cba9177872ac8e7b32c
agustinvd10 <avalenzu@chicagobooth.edu>
2021-04-12 23:35:04 +0000

    wuhu

--- ruby/robot-name/robot_name.rb
+++ ruby/robot-name/robot_name.rb
@@ -6,3 +6,31 @@ To get started with TDD, see the `README.md` file in your
 `ruby/robot-name` directory.
 =end
 
+class Robot
+  attr_reader :name
+
+  class << self
+    def names_used
+      @used ||= []
+    end
+  end
+
+  def initialize
+    loop do
+      @name = generateName
+      break !Robot.names_used.include?(name)
+    end
+    Robot.names_used << @name
+  end
+
+  def reset
+    @name = generateName
+  end
+
+  def generateName
+    (0...2).map { (65 + rand(26)).chr }.join + rand(10).to_s + rand(10).to_s + rand(10).to_s
+  end
+
+  def forget
+  end  
+end
